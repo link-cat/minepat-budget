309a73c378c77c83e9ae1b276d069cd6486e6508
link <smveme@gmail.com>
2024-09-10 08:27:14 +0100

    modify cors-allow-headers

--- projet/projet/settings.py
+++ projet/projet/settings.py
@@ -164,6 +164,20 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
+CORS_ALLOW_HEADERS = (
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+    "content-disposition",
+    "pragma",
+)
+
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
