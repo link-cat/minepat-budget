d4b7e6263be27da06bc9d84b177b90462ceb1421
link <smveme@gmail.com>
2024-09-24 11:14:35 +0100

    add contractualisation app

--- projet/execution/models.py
+++ projet/execution/models.py
@@ -4,6 +4,49 @@ from simple_history.models import HistoricalRecords
 
 from setting.models import Action,Exercice,EtapeExecutionGlob,Tache,Operation,NatureDepense,GroupeDepense,EtapeExecution
 
+CONTRAT_SITUATION_CHOICES = [
+    ("BC:Non executé", "BC:Non executé"),
+    ("BC:Exec. en cours", "BC:Exec. en cours"),
+    ("BC:Executé", "BC:Executé"),
+    (
+        "Marché:Gré-a-Gré,Accord MINMAP attendu",
+        "Marché:Gré-a-Gré,Accord MINMAP attendu",
+    ),
+    ("Marché:ASMI/AMI en cours", "Marché:ASMI/AMI en cours"),
+    ("Marché:DAO non transmis", "Marché:DAO non transmis"),
+    ("Marché:DAO en CCC", "Marché:DAO en CCC"),
+    ("Marché:DAO transmis en CIPM", "Marché:DAO transmis en CIPM"),
+    ("Marché:AO lancé", "Marché:AO lancé"),
+    ("Marché:AO infructueux", "Marché:AO infructueux"),
+    ("Marché:Gré-a-Gré,proc. en cours", "Marché:Gré-a-Gré,proc. en cours"),
+    ("Marché:Analyse offres en cours", "Marché:Analyse offres en cours"),
+    ("Marché:Prop. attrib. en CCC", "Marché:Prop. attrib. en CCC"),
+    ("Marché:Attribué", "Marché:Attribué"),
+    ("Marché:Gré-a-Gré,proj. Marché en CCC", "Marché:Gré-a-Gré,proj. Marché en CCC"),
+    ("Marché:Signé", "Marché:Signé"),
+    ("Marché:OS Dem. trav. notifié", "Marché:OS Dem. trav. notifié"),
+    ("Marché:OS Susp. trav. notifié", "Marché:OS Susp. trav. notifié"),
+    ("Marché:Avenant en cours", "Marché:Avenant en cours"),
+    ("Marché:receptionné", "Marché:receptionné"),
+    ("Marché pluriannuel signé avant 2024", "Marché pluriannuel signé avant 2024"),
+    ("Marché:chantier abandonné", "Marché:chantier abandonné"),
+    ("Marché:resilié", "Marché:resilié"),
+    ("Marché spécial:non signé", "Marché spécial:non signé"),
+    ("Marché spécial:signé", "Marché spécial:signé"),
+    ("Marché spécial:receptionné", "Marché spécial:receptionné"),
+    ("Régie:Accord MINMAP attendu", "Régie:Accord MINMAP attendu"),
+    ("Régie:Exec. en cours", "Régie:Exec. en cours"),
+    ("Régie:Exec. complete", "Régie:Exec. complete"),
+    ("Convention:signature en cours", "Convention:signature en cours"),
+    ("Convention:signée", "Convention:signée"),
+    ("Convention:Commande receptionnée", "Convention:Commande receptionnée"),
+    ("Arriérée:liasse incomplete", "Arriérée:liasse incomplete"),
+    ("Arriérée:liasse complete", "Arriérée:liasse complete"),
+    ("Autre Opération Fonct.:Non executé", "Autre Opération Fonct.:Non executé"),
+    ("Autre Opération Fonct.:Executé", "Autre Opération Fonct.:Executé"),
+    ("Dotation non repartie", "Dotation non repartie"),
+]
+
 class EstExecuteeAction(models.Model):
     action = models.ForeignKey(Action, on_delete=models.CASCADE)
     exercice = models.ForeignKey(Exercice, on_delete=models.CASCADE)
@@ -51,7 +94,9 @@ class EstExecuteeFCPDR(models.Model):
     pourcentage_ord = models.DecimalField(max_digits=4, decimal_places=2)
     pourcentage_RPHY_cp = models.DecimalField(max_digits=4, decimal_places=2)
     gc_category_ressource = models.CharField(max_length=45)
-    contrat_situation_actuelle = models.TextField()
+    contrat_situation_actuelle = models.CharField(
+        max_length=100, choices=CONTRAT_SITUATION_CHOICES, default="BC:Non executé"
+    )
     montant_contrat = models.FloatField()
     date_demarrage_travaux = models.DateTimeField()
     delai_execution_contrat = models.IntegerField()
@@ -77,7 +122,9 @@ class EstExecuteeFCPTDD(models.Model):
     pourcentage_ord = models.DecimalField(max_digits=4, decimal_places=2)
     pourcentage_RPHY_cp = models.DecimalField(max_digits=4, decimal_places=2)
     gc_category_ressource = models.CharField(max_length=45)
-    contrat_situation_actuelle = models.TextField()
+    contrat_situation_actuelle = models.CharField(
+        max_length=100, choices=CONTRAT_SITUATION_CHOICES, default="BC:Non executé"
+    )
     montant_contrat = models.FloatField()
     date_demarrage_travaux = models.DateTimeField()
     delai_execution_contrat = models.IntegerField()
@@ -103,7 +150,9 @@ class EstExecuteeGCAUTRES(models.Model):
     pourcentage_ord = models.DecimalField(max_digits=4, decimal_places=2)
     pourcentage_RPHY_cp = models.DecimalField(max_digits=4, decimal_places=2)
     gc_category_ressource = models.CharField(max_length=45)
-    contrat_situation_actuelle = models.TextField()
+    contrat_situation_actuelle = models.CharField(
+        max_length=100, choices=CONTRAT_SITUATION_CHOICES, default="BC:Non executé"
+    )
     montant_contrat = models.FloatField()
     date_demarrage_travaux = models.DateTimeField()
     delai_execution_contrat = models.IntegerField()
@@ -129,7 +178,9 @@ class EstExecuteeGCSUB(models.Model):
     pourcentage_ord = models.DecimalField(max_digits=4, decimal_places=2)
     pourcentage_RPHY_cp = models.DecimalField(max_digits=4, decimal_places=2)
     gc_category_ressource = models.CharField(max_length=45)
-    contrat_situation_actuelle = models.TextField()
+    contrat_situation_actuelle = models.CharField(
+        max_length=100, choices=CONTRAT_SITUATION_CHOICES, default="BC:Non executé"
+    )
     montant_contrat = models.FloatField()
     date_demarrage_travaux = models.DateTimeField()
     delai_execution_contrat = models.IntegerField()
@@ -180,7 +231,9 @@ class EstExecuteeOperationFDCDR(models.Model):
     pourcentage_ord = models.DecimalField(max_digits=4, decimal_places=2)
     pourcentage_RPHY_cp = models.DecimalField(max_digits=4, decimal_places=2)
     gc_category_ressource = models.CharField(max_length=45)
-    contrat_situation_actuelle = models.TextField()
+    contrat_situation_actuelle = models.CharField(
+        max_length=100, choices=CONTRAT_SITUATION_CHOICES, default="BC:Non executé"
+    )
     montant_contrat = models.FloatField()
     date_demarrage_travaux = models.DateTimeField()
     delai_execution_contrat = models.IntegerField()
