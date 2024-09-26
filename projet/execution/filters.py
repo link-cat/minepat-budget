5404d003a5e7f0f3b3d8c88d37c24d62159b725f
link <smveme@gmail.com>
2024-09-26 12:12:48 +0100

    modify filter

--- projet/execution/filters.py
+++ projet/execution/filters.py
@@ -5,24 +5,24 @@ from .models import EstExecuteeGCSUB, EstExecuteeGCAUTRES
 class EstExecuteeGCSUBFilter(django_filters.FilterSet):
     # Filtrer par titre d'une action
     action = django_filters.CharFilter(
-        field_name="tache__activite__action__code", lookup_expr="icontains"
+        field_name="tache__activite__action__id", lookup_expr="exact"
     )
 
     # Filtrer par titre d'un programme
     programme = django_filters.CharFilter(
-        field_name="tache__activite__action__programme__code",
-        lookup_expr="icontains",
+        field_name="tache__activite__action__programme__id",
+        lookup_expr="exact",
     )
 
     # Filtrer par titre d'une activité
     activite = django_filters.CharFilter(
-        field_name="tache__activite__code", lookup_expr="icontains"
+        field_name="tache__activite__id", lookup_expr="exact"
     )
 
     # Filtrer par titre d'un chapitre
     chapitre = django_filters.CharFilter(
-        field_name="tache__activite__action__programme__chapitre__code",
-        lookup_expr="icontains",
+        field_name="tache__activite__action__programme__chapitre__id",
+        lookup_expr="exact",
     )
 
     class Meta:
@@ -33,24 +33,24 @@ class EstExecuteeGCSUBFilter(django_filters.FilterSet):
 class EstExecuteeGCAutresFilter(django_filters.FilterSet):
     # Filtrer par titre d'une action
     action = django_filters.CharFilter(
-        field_name="tache__activite__action__code", lookup_expr="icontains"
+        field_name="tache__activite__action__id", lookup_expr="exact"
     )
 
     # Filtrer par titre d'un programme
     programme = django_filters.CharFilter(
-        field_name="tache__activite__action__programme__code",
-        lookup_expr="icontains",
+        field_name="tache__activite__action__programme__id",
+        lookup_expr="exact",
     )
 
     # Filtrer par titre d'une activité
     activite = django_filters.CharFilter(
-        field_name="tache__activite__code", lookup_expr="icontains"
+        field_name="tache__activite__id", lookup_expr="exact"
     )
 
     # Filtrer par titre d'un chapitre
     chapitre = django_filters.CharFilter(
-        field_name="tache__activite__action__programme__chapitre__code",
-        lookup_expr="icontains",
+        field_name="tache__activite__action__programme__chapitre__id",
+        lookup_expr="exact",
     )
 
     class Meta:
