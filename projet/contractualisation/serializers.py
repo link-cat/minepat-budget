b46d4c1f7ebe26f9ccdd659e4ae1f3af20798d4a
link <smveme@gmail.com>
2024-12-02 15:35:11 +0100

    add tache infos

--- projet/contractualisation/serializers.py
+++ projet/contractualisation/serializers.py
@@ -20,7 +20,7 @@ class JPMSerializer(serializers.ModelSerializer):
     tache = serializers.SerializerMethodField()
 
     def get_tache(self, obj):
-        return obj.tache.title_fr
+        return obj.tache
 
     class Meta:
         model = JPM
